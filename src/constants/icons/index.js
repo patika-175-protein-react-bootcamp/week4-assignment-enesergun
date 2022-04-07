b1c11313e4fde7c742df8acab6d119a484572068
enesergun1515@gmail.com <63712936+enesergun@users.noreply.github.com>
2022-04-07 17:44:59 +0300

    start button direct to QuestionPage.js

---
+++ src/constants/icons/index.js
@@ -0,0 +1,4 @@
+import HeaderUnderLine from './HeaderUnderLine'
+import StartEllipse from './StartEllipse'
+
+export { StartEllipse, HeaderUnderLine }
--- src/pages/StartPage.js
+++ src/pages/StartPage.js
@@ -1,9 +1,35 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import '../App.css';
+import { StartEllipse } from '../constants/icons'
+import { HeaderUnderLine } from '../constants/icons'
 
 function StartPage() {
   return (
-    <div>StartPage</div>
+    <div className='startPage'>
+        <div className="title">Mathematics Game</div>
+        <HeaderUnderLine />
+
+        <div className="totalData">
+            <div className="totalPoint">
+                <p>Total Point: 129</p>
+            </div>
+            <div className="totalQuestions">
+                <p>Total Questions: 40</p>
+            </div>
+            <div className="correctAnswers">
+                <p>Correct Answers: 32</p>
+            </div>
+        </div>
+
+        <div className="startButton">
+            <StartEllipse />
+            <span>
+                <Link to="/question">Start</Link>
+            </span>
+            
+        </div>
+    </div>
   )
 }
 
