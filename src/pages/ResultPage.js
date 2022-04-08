5da9d5f818920d0e031ef03276a1fcd1d812103e
enesergun1515@gmail.com <63712936+enesergun@users.noreply.github.com>
2022-04-08 15:36:11 +0300

    click restart button turn home start page in result page

--- src/pages/ResultPage.js
+++ src/pages/ResultPage.js
@@ -1,33 +1,43 @@
 import React, {useState, useEffect} from 'react'
+import {Link} from 'react-router-dom'
 
 
 function ResultPage() {
   /* total  */
-  const [totalPoint, setTotalPoint] = useState(0);
+  const [totalPoint, settTotalPoint] = useState(JSON.parse(localStorage.getItem('totalPoint')))
 
-  let score = JSON.parse(localStorage.getItem('score'));
-  
-  console.log(typeof(score));
-  
-  
+  /* let total = JSON.parse(localStorage.getItem('totalPoint'))  */
+  let tourPoint = JSON.parse(localStorage.getItem('score'));
   useEffect(() => {
-    setTotalPoint(totalPoint + score);
+    settTotalPoint(tourPoint + totalPoint)
   }, [])
   
-  
-  console.log(totalPoint);
+  localStorage.setItem('totalPoint', totalPoint);
 
+  let resultsArr = JSON.parse(localStorage.getItem('results')).slice(0, 10).reverse();
+
+  let correctAnswers = resultsArr.filter(item => item[1] === 'correct');
+  console.log(correctAnswers);
 
   return (
     <div className='resultPage'>
       <div className="final">
         <div className="title">Final</div>
-        <div className="score">Point : {JSON.parse(localStorage.getItem('score'))}</div>
-        <div className="questions">Questions : 0</div>
-        <div className="correctAnswers">Correct Answers: 0</div>
+        <div className="score">Point : {tourPoint}</div>
+        <div className="questions">Questions : {resultsArr.length}</div>
+        <div className="correctAnswers">Correct Answers: {correctAnswers.length}</div>
+        <button> <Link to="/">Restart</Link></button>
       </div>
       <div className="allQuestion">
         <div className="title">All Question</div>
+        {
+          resultsArr?.map((item, index) => (
+            <div className="question" key={index}>
+              <span>{item[0]}</span>
+              <span className="answer">{item[1]}</span>
+            </div>
+          ))
+        }
       </div>
     </div>
   )
